01e60e20f6b648537f3f1a16e9a11f23c240f8a2
Luigi Mannoni <hello@luigimannoni.com>
2021-08-28 17:54:36 +0100

    Test using axios

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown';
+import axios from 'axios';
 
 export default function Markdown({ file }) {
   const [markdown, setMarkdown] = useState('');
 
   useEffect(() => {
-    fetch(file)
-      .then((res) => res.text())
-      .then((text) => setMarkdown(text));
+    async function getMD() {
+      const text = await axios.get(file);
+      setMarkdown(text);
+    }
+    getMD();
   }, [file]);
 
   return (
