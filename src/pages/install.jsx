2796a155c1b1344d8f8dd5d804f3ef153a3a24dd
Luigi Mannoni <hello@luigimannoni.com>
2021-08-28 18:39:32 +0100

    Try cloning the homepage...

--- src/pages/install.jsx
+++ src/pages/install.jsx
@@ -2,16 +2,40 @@ import React from 'react';
 import {
   Container, Row, Col,
 } from 'react-bootstrap';
+import ReactPlayer from 'react-player/youtube';
 import Markdown from '../components/Markdown';
 
-import MD_MAIN from '../content/install/main.md';
+import MD_MAIN from '../content/homepage/main.md';
+import MD_FAQS from '../content/homepage/faqs.md';
 
 export default function InstallPage() {
   return (
     <Container>
-      <Row className="py-1 justify-content-center">
-        <Col xs={10} md={8} lg={6}>
+      <Row className="py-1">
+        <Col>
+          <h1>What is The Alpha Project?</h1>
+        </Col>
+      </Row>
+      <Row className="py-1">
+        <Col lg={6}>
           <Markdown file={MD_MAIN} />
+        </Col>
+        <Col lg={6}>
+          {/* Dodgykebaabs video */}
+          <div className="ratio ratio-16x9">
+            <ReactPlayer
+              url="https://www.youtube.com/watch?v=ZCpwYZeIBC0&t=29"
+              controls
+              pip
+              width="100%"
+              height="100%"
+            />
+          </div>
+        </Col>
+      </Row>
+      <Row className="py-1">
+        <Col>
+          <Markdown file={MD_FAQS} />
         </Col>
       </Row>
     </Container>
