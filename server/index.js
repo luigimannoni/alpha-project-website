fc86560b8f195960b3b4ebefb6c846a94d5c7594
Luigi Mannoni <hello@luigimannoni.com>
2021-09-27 13:19:50 +0100

    Lint code and add missing dep

--- server/index.js
+++ server/index.js
@@ -1,40 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors')
+const cors = require('cors');
 require('dotenv').config();
 
 const mysql = require('mysql');
 
-
 const app = express();
 
-//Middlewares
+// Middlewares
 app.use(bodyParser.json());
 app.use(cors({
-  origin: '*'
+  origin: '*',
 }));
-
 
 const PORT = process.env.PORT || 3500;
 
 const connection = mysql.createConnection({
-  host     : process.env.HOST,
-  user     : process.env.USER,
-  password : process.env.PASSWORD,
-  database : process.env.DATABASE,
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
 
-connection.connect( error => {
-    if(error) throw error;
-    console.log('DB connection established');
+connection.connect((error) => {
+  if (error) throw error;
+  console.log('DB connection established');
 });
 
-//Routes
+// Routes
 app.post('/api/account/createAccount', (req, res) => {
-    const {username, s, v} = req.body;
-    const format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+  const { username, s, v } = req.body;
+  const format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
 
-    const sql = `INSERT INTO account
+  const sql = `INSERT INTO account
             (
                 username, s, v
             )
@@ -42,31 +40,17 @@ app.post('/api/account/createAccount', (req, res) => {
             (
                 ?, ?, ?
             )`;
-    
-    if(!username.match(format)) {
-      connection.query(sql, [username, s, v], function(err, rows, fields) {
-        if (err) throw err;
-        res.send('Your account was successfully created. Please set your realmlist to `set realmlist logon1.thealphaproject.eu`! Have fun!');
-      });
-    }
 
-    else if(username.match(format)){
-      res.send("Remove odd characters to continue!");
-      return;
-    }
-
-    else if(username.length > 16){
-      res.send("The given Username is too long! Please use a username with a maximum of 16 characters!");
-      return;
-    }
-    
-   
-    
-})
-
+  if (!username.match(format)) {
+    connection.query(sql, [username, s, v], (err, rows, fields) => {
+      if (err) throw err;
+      res.send('Your account was successfully created. Please set your realmlist to `set realmlist logon1.thealphaproject.eu`! Have fun!');
+    });
+  } else if (username.match(format)) {
+    res.send('Remove odd characters to continue!');
+  } else if (username.length > 16) {
+    res.send('The given Username is too long! Please use a username with a maximum of 16 characters!');
+  }
+});
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
-
-
-
-
